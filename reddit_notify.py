ed00d71b2f00ec77b5b3b1b806b2e24eeadc60fb
ajnagashima <arthurjnagashima@gmail.com>
2019-02-25 21:08:39 +0000

    Add the beginning of input analysis for config

--- reddit_notify.py
+++ reddit_notify.py
@@ -1,4 +1,45 @@
 # package praw
 import praw
 # package pyyaml
-from yaml import load, dump
+import yaml
+
+def main():
+    config = importConfig()
+    creds = []
+    subreddits = []
+    print("Extracting credentials...", end = "")
+    try:
+        creds = config["credentials"]
+    except:
+        error("credentials not found, use keyword 'credentials' in config.yaml")
+    print("DONE.")
+
+    print("Extracting subreddits...", end = "")
+    try:
+        subreddits = config["subreddits"]
+    except:
+        error("subreddits not found, use keyword 'subreddits' in config.yaml")
+    print("DONE.")
+
+    print(creds)
+    print(subreddits)
+def importConfig():
+    print("Importing config.yaml...", end = "")
+    try:
+        with open("conf.yaml", 'r') as conf:
+            try:
+                config = yaml.load(conf)
+                print("DONE.")
+                return config
+            except yaml.YAMLError as exc:
+                error(exc)
+    except Exception as exc:
+        error(exc)
+
+
+def error(msg):
+    print("\nERROR: ", msg)
+    exit()
+
+if __name__ == "__main__":
+    main()
