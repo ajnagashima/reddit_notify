a421a1e3910bc2ee8f6b4ebbf970a74f834f5b95
ajnagashima <arthurjnagashima@gmail.com>
2019-02-26 15:13:58 -0500

    Add functionality to sender

--- notify.py
+++ notify.py
@@ -20,19 +20,20 @@ def crawl(sub, reddit, sender, run_event):
 
         posts = [p for p in posts if p[1]+sub.post_life_time > time.time()]
 
-        notify_new(sub, new_posts, sender)
+        if len(new_posts):
+            notify_new(sub, new_posts, sender)
         last_notify = time.time()
         while run_event.is_set() and time.time() < life_time and time.time() < last_notify+sub.crawl_rate:
             time.sleep(1)
-    
+
     new_posts = check_new(sub, reddit, posts)
 
     if sub.store_on_notify:
         for post in new_posts:
             file.write("{}\n".format(json.dumps(post)))
 
-    posts = [p for p in posts if p[1]+sub.post_life_time > time.time()]
-    notify_new(sub, posts, sender)
+    if len(new_posts):
+    notify_new(sub, new_posts, sender)
 
     notify_done(sub, sender)
     if sub.store_on_notify:
@@ -50,11 +51,10 @@ def check_new(sub, reddit, posts):
             posts = [(k, v) if (k!=post.id) else (post.id, time.time()) for (k,v) in posts]
         elif any(word in post.title or word in post.selftext for word in sub.key_words):
             posts.append((post.id, time.time()))
-            print(vars(post))
             new_posts.append({
                 "title":post.title,
-                "body":post.selftext,
-                "time_found": time.time()
+                "url":post.url,
+                "time":post.created_utc
             })
     return new_posts
 
--- sender.py
+++ sender.py
@@ -1,7 +1,36 @@
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
 class Sender:
-    # TODO
     def send_posts(self, toaddr, subreddit, posts):
-        print("send posts called")
+        msg = MIMEMultipart()
+        msg['From'] = self.email_addr
+        msg['To'] = toaddr
+        msg['Subject'] = "New posts from %s"%subreddit.name
+        msg['Date'] = formatdate(localtime=True)
+
+        body = ""
+        for post in posts:
+            body = (body + "Posted at " + 
+                str(formatdate(post['time'], localtime=True)) +":\n"+ 
+                str(post['title']) + "\n" + 
+                str(post['url']) + "\n\n")
+
+        msg.attach(MIMEText(body, 'plain'))
+        server = []
+        try:
+            server = smtplib.SMTP(self.mail_server, 25)
+        except:
+            print("FATAL: no mail serveri conf")
+            exit(1)
+        server.starttls()
+        text = msg.as_string()
+        server.sendmail(self.email_addr, toaddr, text)
+        server.quit()
+
 
     def send_msg(self, toaddr, subject, body):
         print("send message called")
@@ -11,8 +40,7 @@ class Sender:
             self.email_addr = data['email_addr']
         except:
             print("WARN: no email address in conf")
-        try:
-            self.sms_number = data['sms_number']
+        try: 
+            self.mail_server = data['mail_server']
         except:
-            print("WARN: no sms number in conf")
-
+            print("WARN: no mail server in conf")
